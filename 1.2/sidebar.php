e309e4630fa262c552b7f03cd93aa3cfb131df25
themedropbox <themedropbox@8c925393-aaf8-4f5c-984e-1f1965effe94>
2011-04-15 05:57:45 +0000

    New version of BlankSlate - 1.2

---
+++ 1.2/sidebar.php
@@ -0,0 +1,1 @@
+<div id="sidebar"><?php if ( is_sidebar_active('primary-widget-area') ) : ?><div id="primary" class="widget-area"><ul class="sid"><?php dynamic_sidebar('primary-widget-area'); ?></ul></div><?php endif; ?></div>
