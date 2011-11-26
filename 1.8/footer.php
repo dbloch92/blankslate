4b8adb7aac0b5bb68743d1dedbdcca2d5dd88fb8
themedropbox <themedropbox@8c925393-aaf8-4f5c-984e-1f1965effe94>
2011-11-26 18:50:23 +0000

    New version of BlankSlate - 1.8

---
+++ 1.8/footer.php
@@ -0,0 +1,10 @@
+</div>
+<div id="footer">
+<p id="copyright">
+&copy; <?php echo date("Y") ?> <?php bloginfo( 'name' ) ?>. All Rights Reserved. Theme By: <a href="http://tidythemes.com/">TidyThemes</a>
+</p>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
